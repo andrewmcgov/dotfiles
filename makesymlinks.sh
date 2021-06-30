5b66b8e1d3424f53c3c50a0ea4d2009e931fdc33
Andrew McGoveran <andrew.mcgoveran@shopify.com>
2021-06-30 17:40:32 -0400

    Starting custom prompt

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".bashrc .zshrc .hyper.js"    # list of files/folders to symlink in homedir
+files=".bashrc .zshrc .hyper.js .gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
